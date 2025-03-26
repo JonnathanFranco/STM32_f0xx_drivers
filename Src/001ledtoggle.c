e14c61f330d0cdb48755a0b10b43dc20896c2b16
PC-1 <PC-1@DESKTOP-A0OE4G1>
2025-03-26 22:12:13 +1100

    delete and put back syscalls.c and sysmem.c

---
+++ Src/001ledtoggle.c
@@ -0,0 +1,19 @@
+/*
+ * 001ledtoggle.c
+ *
+ *  Created on: Mar 26, 2025
+ *      Author: PC-1
+ */
+
+#include "stm32f070xx.h"
+
+
+
+int main(void)
+{
+	GPIO_Handle_t Gpioled;
+	GPIO_Init(pGPIOHandle)
+
+
+	return 0;
+}
