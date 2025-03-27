6aa78b9147786abb25e95bc55570f25250668619
z3546658 <z3546658@131.236.140.181>
2025-03-27 14:08:18 +1100

    LED_toggle

--- Src/001ledtoggle.c
+++ Src/001ledtoggle.c
@@ -5,14 +5,14 @@
  *      Author: PC-1
  */
 
-#include "stm32f070xx.h"
+#include "stm32f070xx_gpio_driver.h"
 
 
 
 int main(void)
 {
-	GPIO_Handle_t Gpioled;
-	GPIO_Init(pGPIOHandle)
+//	GPIO_Handle_t Gpioled;
+//	GPIO_Init(pGPIOHandle)
 
 
 	return 0;
