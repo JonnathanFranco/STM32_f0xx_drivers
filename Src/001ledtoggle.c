312b9ea8f9b779b5fd0f1e6899ccc76e1473f8e8
z3546658 <z3546658@L-2QJPTT3.ad.unsw.edu.au>
2025-03-27 16:09:49 +1100

    NO WORKING TOGGLE

--- Src/001ledtoggle.c
+++ Src/001ledtoggle.c
@@ -6,7 +6,7 @@
  */
 
 #include "stm32f070xx.h"
-#include "stm32f070xx_gpio_driver.h"
+//#include "stm32f070xx_gpio_driver.h"
 
 void delay(void)
 {
@@ -17,25 +17,27 @@ void delay(void)
 
 int main(void)
 {
-	GPIO_Handle_t Gpioled;
 
-	Gpioled.pGPIOx = GPIOA;
+	GPIO_Handle_t GpioLed;
+
+	GpioLed.pGPIOx = GPIOA;
+
+	GpioLed.GPIO_PinConfig->GPIO_PinNumber = GPIO_PIN_NO_5;
+	GpioLed.GPIO_PinConfig->GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig->GPIO_PinSpeed = GPIO_SPEED_HIGH;
+	GpioLed.GPIO_PinConfig->GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GpioLed.GPIO_PinConfig->GPIO_PinPuPdControl = GPIO_NO_PUPD;
 
 
-	Gpioled.GPIO_PinConfig->GPIO_PinNumber = GPIO_PIN_NO_5;
-	Gpioled.GPIO_PinConfig->GPIO_PinMode = GPIO_MODE_OUT;
-	Gpioled.GPIO_PinConfig->GPIO_PinSpeed = GPIO_SPEED_HIGH;
-	Gpioled.GPIO_PinConfig->GPIO_PinOPType = GPIO_OP_TYPE_PP;
-	Gpioled.GPIO_PinConfig->GPIO_PinPuPdControl = GPIO_NO_PUPD;
 
 	GPIO_PeriClockControl (GPIOA, ENABLE);
 
-	GPIO_Init(&Gpioled);
+	GPIO_Init(&GpioLed);
 
 	while(1)
 	{
 
-		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_5);
+		GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_5);
 		delay();
 	}
 
